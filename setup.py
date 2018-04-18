ebcef8114b611cf79d8ece0840b7ec7023dbd0ae
Sean Harrison <sah@bookgenesis.com>
2018-04-18 09:59:24 -0500

    0.7.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 config = {
   "name": "bq",
-  "version": "0.6.0",
+  "version": "0.7.0",
   "description": "Queue library",
   "url": "https://github.com/BlackEarth/bq",
   "author": "Sean Harrison",
